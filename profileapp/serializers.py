4d658da83cd73eac5dc16f03bff29977d242b220
MinJi Kang <86655689+100100001001@users.noreply.github.com>
2021-12-15 15:38:33 +0900

    image upload javascript update

--- profileapp/serializers.py
+++ profileapp/serializers.py
@@ -4,4 +4,4 @@ from profileapp.models import Profile
 class ProfileSerializer(ModelSerializer):
     class Meta:
         model = Profile
-        fields = ['nickname', 'image', 'message']
+        fields = ['id', 'nickname', 'image', 'message']
