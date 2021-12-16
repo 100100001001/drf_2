0b9cc2a70d565be35b73bb5a15a0bc3804cec93d
MinJi Kang <86655689+100100001001@users.noreply.github.com>
2021-12-16 13:26:25 +0900

    Account retrieve template update

--- accountapp/static/accountapp/js/retrieve.js
+++ accountapp/static/accountapp/js/retrieve.js
@@ -1,21 +1,20 @@
+function initialize(pk) {
+    axios.get('/profiles/' + pk)
+        .then(function (response) {
+            // handle success
+            console.log(response);
 
-function initialize (pk) {
+            document.getElementById('nickname').innerHTML = response.data['nickname'];
+            document.getElementById('message').innerHTML = response.data['message'];
 
-    axios.get('/accounts/' + pk)
-      .then(function (response) {
-        // handle success
-        console.log(response);
+            document.getElementById('image').src = response.data['image'];
 
-        document.getElementById('username').innerHTML = response.data['username'];
-        document.getElementById('date_joined').innerHTML = response.data['date_joined'];
-
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      })
-      .then(function () {
-        // always executed
-      });
-
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .then(function () {
+            // always executed
+        });
 }
